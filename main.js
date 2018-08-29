41bb5dee69031493315a8391c3fa1d240ac9546e
Engrid Hernandez <engrid.hernandez@gmail.com>
2018-08-29 17:29:07 -0400

    almost functional

--- main.js
+++ main.js
@@ -1,18 +1,60 @@
 //user can select an empty box
+
 //user can win if 3 box's in a row are selected
 //users can tie if all box's are full
 
-//add play x/o class
+// possible win combinations:
+// 1,2,3
+// 1,4,7
+// 1,5,9
+// 4,5,6
+// 2,5,8
+// 3,6,9
+// 7,8,9
+// 3,5,7
+
+//add player x/o class
 //remove unselected class
+//event listener for clicking of boxes
 
-let emptyBox  = document.getElementsByClassName('unselected-box')
-let xSelected = document.getElementsByClassName('selected-by-x')
-let oSelected = document.getElementsByClassName('selected-by-o')
+document.addEventListener("DOMContentLoaded",()=>{
+  let emptyBox  = document.getElementsByClassName('unselectedBox')
+  //player/turn count
+  let player    = 0
+  //check if there's a winner
+  let xSelected = document.getElementsByClassName('selectedByX')
+  let oSelected = document.getElementsByClassName('selectedByO')
 
+  document.addEventListener('click', ()=>{
+    //if the box is selected - do nothing - still current player's turn
 
-if(xSelected || oSelected){
+  });
 
-}
+  //check box's status so as not to override
+  let boxElement = document.getElementsByClassName('box')
 
-//check if there's a winner
-//check box's status so as not to override
+  boxSelection = boxElement.onclick;
+  function boxSelection(){
+
+    if(boxElement.classList.contains('unselectedBox')){
+      checkPlayerAndSelectBox()
+      player++
+    }else{
+      h2.innerHTML('Select a box that is not already in play')
+    }
+
+  }
+
+  function checkPlayerAndSelectBox(){
+    //selection by X (even)
+    if(player % 2 == 0){
+      boxElement.classlist.replace('unselectedBox, selectedByX')
+    //selection by O (odd)
+    }else{
+      boxElement.classlist.replace('unselectedBox, selectedByO')
+    }
+    //box is not empty
+    boxElement.classlist.add('selectedBox')
+  }
+
+});

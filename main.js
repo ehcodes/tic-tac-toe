c5e7af707018e7cce696b984c231bd43c404ca33
Engrid Hernandez <engrid.hernandez@gmail.com>
2018-08-29 16:09:12 -0400

    first commit

---
+++ main.js
@@ -0,0 +1,18 @@
+//user can select an empty box
+//user can win if 3 box's in a row are selected
+//users can tie if all box's are full
+
+//add play x/o class
+//remove unselected class
+
+let emptyBox  = document.getElementsByClassName('unselected-box')
+let xSelected = document.getElementsByClassName('selected-by-x')
+let oSelected = document.getElementsByClassName('selected-by-o')
+
+
+if(xSelected || oSelected){
+
+}
+
+//check if there's a winner
+//check box's status so as not to override

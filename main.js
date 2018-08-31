76b9bb2574679278ddc0f89d36c3f26170c2a235
Engrid Hernandez <engrid.hernandez@gmail.com>
2018-08-30 20:16:09 -0400

    some day functional

--- main.js
+++ main.js
@@ -3,23 +3,13 @@
 //user can win if 3 box's in a row are selected
 //users can tie if all box's are full
 
-// possible win combinations:
-// 1,2,3
-// 1,4,7
-// 1,5,9
-// 4,5,6
-// 2,5,8
-// 3,6,9
-// 7,8,9
-// 3,5,7
-
 //add player x/o class
 //remove unselected class
 //event listener for clicking of boxes
 
 document.addEventListener("DOMContentLoaded",()=>{
   let emptyBox  = document.getElementsByClassName('unselectedBox')
-  //player/turn count
+  //player/turn count - X=Even - O=Odd
   let player    = 0
   //check if there's a winner
   let xSelected = document.getElementsByClassName('selectedByX')
@@ -27,12 +17,13 @@ document.addEventListener("DOMContentLoaded",()=>{
 
   document.addEventListener('click', ()=>{
     //if the box is selected - do nothing - still current player's turn
-
+    boxSelection();
+    checkForWin();
   });
 
   //check box's status so as not to override
   let boxElement = document.getElementsByClassName('box')
-  boxSelection = boxElement.onclick;
+   boxElement.onclick = boxSelection;
 
   function boxSelection(){
 
@@ -57,7 +48,34 @@ document.addEventListener("DOMContentLoaded",()=>{
     // boxElement.classlist.add('selectedBox')
   }
 
+
+
   function checkForWin(){
+
+    let boxOne    = document.getElementsByClassName('box1')
+    let boxTwo    = document.getElementsByClassName('box2')
+    let boxThree  = document.getElementsByClassName('box3')
+    let boxFour   = document.getElementsByClassName('box4')
+    let boxFive   = document.getElementsByClassName('box5')
+    let boxSix    = document.getElementsByClassName('box6')
+    let boxSeven  = document.getElementsByClassName('box7')
+    let boxEight  = document.getElementsByClassName('box8')
+    let boxNine   = document.getElementsByClassName('box9')
+
+
+
+    if(boxOne.classList.contains('selectedByX') && boxTwo.classList.contains('selectedByX') && boxThree.classList.contains('selectedByX')){
+      h2.innerHTML("You've won the game!")
+    }
+    // possible win combinations:
+    // 1,2,3
+    // 1,4,7
+    // 1,5,9
+    // 4,5,6
+    // 2,5,8
+    // 3,6,9
+    // 7,8,9
+    // 3,5,7
 
   }
 
